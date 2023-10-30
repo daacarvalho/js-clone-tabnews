7180b1b7e9ead323fc387860575e538c20cc8888
Daniel Carvalho <68928288+daacarvalho@users.noreply.github.com>
2023-10-30 23:54:01 +0000

    alterando texto para segundo deploy

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Clone do Site do tabnews - Daniel Carvalho</h1>
+    return <h1>Clone do Site do tabnews - Daniel Carvalho - Segundo Deploy</h1>
 }
 
 export default Home;
