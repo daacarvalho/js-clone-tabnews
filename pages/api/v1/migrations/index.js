fa72024c2c360453a0f52265889d54450968f5de
Daniel Carvalho <68928288+daacarvalho@users.noreply.github.com>
2025-03-05 22:20:48 +0000

    refactor: use next-connect in /migrations and /status

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,8 +1,25 @@
 import migrationRunner from "node-pg-migrate";
 import { resolve } from "node:path";
 import database from "infra/database.js";
+import { createRouter } from "next-connect";
+import controller from "infra/controller";
+const router = createRouter();
 
-export default async function migrations(request, response) {
+router.get(getHandler);
+router.post(postHandler);
+
+export default router.handler(controller.errorHandlers);
+
+const defaultMigrationsOptions = {
+  databaseUrl: process.env.DATABASE_URL,
+  dryRun: true,
+  dir: resolve("infra", "migrations"),
+  direction: "up",
+  verbose: true,
+  migrationsTable: "pgmigrations",
+};
+
+async function getHandler(request, response) {
   const allowedMethods = ["GET", "POST"];
 
   if (!allowedMethods.includes(request.method)) {
@@ -16,35 +33,40 @@ export default async function migrations(request, response) {
   try {
     dbClient = await database.getNewClient();
 
-    const defaultMigrationsOptions = {
-      dbClient: dbClient,
-      databaseUrl: process.env.DATABASE_URL,
-      dryRun: true,
-      dir: resolve("infra", "migrations"),
-      direction: "up",
-      verbose: true,
-      migrationsTable: "pgmigrations",
-    };
+    const pendingMigrations = await migrationRunner({
+      ...defaultMigrationsOptions,
+      dbClient,
+    });
+    return response.status(200).json(pendingMigrations);
+  } finally {
+    await dbClient.end();
+  }
+}
 
-    if (request.method === "GET") {
-      const pendingMigrations = await migrationRunner(defaultMigrationsOptions);
-      return response.status(200).json(pendingMigrations);
+async function postHandler(request, response) {
+  const allowedMethods = ["GET", "POST"];
+
+  if (!allowedMethods.includes(request.method)) {
+    return response.status(405).json({
+      error: `Method "${request.method}" not allowed.`,
+    });
+  }
+
+  let dbClient;
+
+  try {
+    dbClient = await database.getNewClient();
+
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationsOptions,
+      dbClient,
+      dryRun: false,
+    });
+
+    if (migratedMigrations.length > 0) {
+      return response.status(201).json(migratedMigrations);
     }
-
-    if (request.method === "POST") {
-      const migratedMigrations = await migrationRunner({
-        ...defaultMigrationsOptions,
-        dryRun: false,
-      });
-
-      if (migratedMigrations.length > 0) {
-        return response.status(201).json(migratedMigrations);
-      }
-      return response.status(200).json(migratedMigrations);
-    }
-  } catch (error) {
-    console.error(error);
-    throw error;
+    return response.status(200).json(migratedMigrations);
   } finally {
     await dbClient.end();
   }
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,44 +1,37 @@
+import { createRouter } from "next-connect";
 import database from "infra/database.js";
-import { InternalServerError } from "infra/errors.js";
+import controller from "infra/controller";
 
-async function status(request, response) {
-  try {
-    const updatedAt = new Date().toISOString();
-    const dataBaseName = process.env.POSTGRES_DB;
+const router = createRouter();
 
-    const dataBaseVersionQuery = await database.query("SHOW server_version;");
-    const dataBaseVersion = dataBaseVersionQuery.rows[0].server_version;
+router.get(getHandler);
 
-    const showMaxConnectionsQuery = await database.query(
-      "SHOW MAX_CONNECTIONS;",
-    );
-    const maxConnections = showMaxConnectionsQuery.rows[0].max_connections;
+export default router.handler(controller.errorHandlers);
 
-    const openedConnectionsQuery = await database.query({
-      text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
-      values: [dataBaseName],
-    });
-    const openedConnections = openedConnectionsQuery.rows[0].count;
+async function getHandler(request, response) {
+  const updatedAt = new Date().toISOString();
+  const dataBaseName = process.env.POSTGRES_DB;
 
-    response.status(200).json({
-      updated_at: updatedAt,
-      dependencies: {
-        database: {
-          database_version: dataBaseVersion,
-          max_connections: parseInt(maxConnections),
-          opened_connections: openedConnections,
-        },
+  const dataBaseVersionQuery = await database.query("SHOW server_version;");
+  const dataBaseVersion = dataBaseVersionQuery.rows[0].server_version;
+
+  const showMaxConnectionsQuery = await database.query("SHOW MAX_CONNECTIONS;");
+  const maxConnections = showMaxConnectionsQuery.rows[0].max_connections;
+
+  const openedConnectionsQuery = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [dataBaseName],
+  });
+  const openedConnections = openedConnectionsQuery.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        database_version: dataBaseVersion,
+        max_connections: parseInt(maxConnections),
+        opened_connections: openedConnections,
       },
-    });
-  } catch (error) {
-    const publicErrorObject = new InternalServerError({
-      cause: error,
-    });
-
-    console.log("\n Erro dentro do catch do controller.");
-    console.error(publicErrorObject);
-    response.status(500).json(publicErrorObject);
-  }
+    },
+  });
 }
-
-export default status;
