9a31f40c986258273eccf3fc97f88e4c09623120
Daniel Carvalho <68928288+daacarvalho@users.noreply.github.com>
2026-05-24 00:34:23 +0000

    feat(add `features` column to `users` table): add features column to users table

---
+++ infra/migrations/1779580176136_create-add-features-to-users.js
@@ -0,0 +1,12 @@
+exports.up = (pgm) => {
+    pgm.addColumn("users", {
+        features: {
+            type: "varchar[]",
+            default: "{}",
+            notNull: true
+        }
+    })
+};
+
+
+exports.down = false;
